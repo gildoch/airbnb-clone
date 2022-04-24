13735b4ca2551bdf4a8af37d141218fe85e55d8e
Gildo Costa Chauze <gildochauze@gmail.com>
2022-04-24 20:10:12 +0200

    Data set changed

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -97,20 +97,6 @@ const Data = [
     location: "Online",
     openSpots: 0,
   },
-  {
-    id: 2,
-    title: "Learn Wedding Photography",
-    description:
-      "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
-    price: 125,
-    coverImg: "./assets/wedding-photography.png",
-    stats: {
-      rating: 5.0,
-      reviewCount: 30,
-    },
-    location: "Online",
-    openSpots: 27,
-  },
 ];
 
 export default Data;
